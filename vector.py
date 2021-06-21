1fa2111f2c3320e3ca40a8b4dc2aa40ff6a17dbd
Subash Mylraj <subashmylraj@gmail.com>
2021-06-21 16:29:59 +0300

    points and vectors can be initialized by points, vectors and tuples | Added functionality for interactions between vectors and points

--- vector.py
+++ vector.py
@@ -1,22 +1,33 @@
-WARNING = 0
+WARNING = 1
 
 class point2d:
 
-	def __init__(self, point):
-		assert len(point) == 2, "Point needs to be 2-dimensional"
-		self.point = point
+	def __init__(self, pt):
+		if isinstance(pt, vector2d):
+			self.point = pt.vector
+		if isinstance(pt, tuple):
+			assert len(pt) == 2, "vector must be 2 dimensional."
+			self.point = pt
 
 	def __add__(self, b):
 		if isinstance(b, point):
 			return point((self.point[0]+b.point[0], self.point[1]+b.point[1], b.point[2]))
 		if isinstance(b, point2d):
 			return point((self.point[0]+b.point[0], self.point[1]+b.point[1]))
+		if isinstance(b, vector):
+			return vector((self.point[0]+b.vector[0], self.point[1]+b.vector[1], b.vector[2]))
+		if isinstance(b, vector2d):
+			return vector((self.point[0]+b.vector[0], self.point[1]+b.vector[1]))
 
 	def __sub__(self, b):
 		if isinstance(b, point):
 			return point((self.point[0]-b.point[0], self.point[1]-b.point[1], b.point[2]))
 		if isinstance(b, point2d):
 			return point((self.point[0]-b.point[0], self.point[1]-b.point[1]))
+		if isinstance(b, vector):
+			return vector((self.point[0]-b.vector[0], self.point[1]-b.vector[1], b.vector[2]))
+		if isinstance(b, vector2d):
+			return vector((self.point[0]-b.vector[0], self.point[1]-b.vector[1]))
 
 	def __str__(self):
 		return f"{self.point}"
@@ -24,29 +35,50 @@ class point2d:
 class point(object):
 
 	def __new__(cls, pt):
-		if len(pt) == 2:
-			if WARNING:
-				print("Creating point2d object")
+		if isinstance(pt, tuple):
+			if len(pt) == 2:
+				if WARNING:
+					print("Creating point2d object")
+				return point2d(pt)
+			if len(pt) == 3:
+				if WARNING:
+					print("Creating point3d object")
+				return super(point, cls).__new__(cls)
+			assert len(pt) in [2, 3], "point must be either 2 or 3 dimensional."
+		if isinstance(pt, vector2d):
 			return point2d(pt)
-		if len(pt) == 3:
-			if WARNING:
-				print("Creating point3d object")
+		if isinstance(pt, vector):
+			return super(point, cls).__new__(cls)
+		if isinstance(pt, point2d):
 			return super(point, cls).__new__(cls)
 
-	def __init__(self, point):
-		self.point = point
+	def __init__(self, pt):
+		if isinstance(pt, vector):
+			self.point = pt.vector
+		if isinstance(pt, point2d):
+			self.point = pt.point + (0,)
+		if isinstance(pt, tuple):
+			self.point = pt
 
 	def __add__(self, b):
 		if isinstance(b, point):
 			return point((self.point[0]+b.point[0], self.point[1]+b.point[1], self.point[2]+b.point[2]))
 		if isinstance(b, point2d):
 			return point((self.point[0]+b.point[0], self.point[1]+b.point[1], self.point[2]))
+		if isinstance(b, vector):
+			return vector((self.point[0]+b.vector[0], self.point[1]+b.vector[1], self.point[2]+b.vector[2]))
+		if isinstance(b, vector2d):
+			return vector((self.point[0]+b.vector[0], self.point[1]+b.vector[1], self.point[2]+0))
 
 	def __sub__(self, b):
 		if isinstance(b, point):
 			return point((self.point[0]-b.point[0], self.point[1]-b.point[1], self.point[2]-b.point[2]))
 		if isinstance(b, point2d):
 			return point((self.point[0]-b.point[0], self.point[1]-b.point[1], self.point[2]))
+		if isinstance(b, vector):
+			return vector((self.point[0]-b.vector[0], self.point[1]-b.vector[1], self.point[2]-b.vector[2]))
+		if isinstance(b, vector2d):
+			return vector((self.point[0]-b.vector[0], self.point[1]-b.vector[1], self.point[2]-0))
 
 	def __str__(self):
 		return f"{self.point}"
@@ -54,21 +86,39 @@ class point(object):
 class vector2d:
 
 	vector = None
-	def __init__(self, *args):
+	def __init__(self, v, u=None):
 		
-		if len(args) == 1:
-			assert len(args[0]) == 2, "Vector needs to be 2-dimensional"
-			self.vector = args[0]
-
-		if len(args) == 2:
-			v = (args[0] - args[1])
-			self.vector = (v.point[0], v.point[1])
+		if u == None:
+			if isinstance(v, point2d):
+				self.vector = v.point
+			if isinstance(v, tuple):
+				assert len(v) == 2, "vector must be 2 dimensional."
+				self.vector = v
+		else:
+			if isinstance(v, point2d):
+				self.vector = (v-u).point
+			if isinstance(v, tuple):
+				self.vector = (v[0]-u[0], v[1]-u[1])
 
 	def __add__(self, b):
-		return vector2d((self.vector[0]+b.vector[0], self.vector[1]+b.vector[1]))
+		if isinstance(b, point):
+			return vector((self.vector[0]+b.point[0], self.vector[1]+b.point[1], b.point[2]))
+		if isinstance(b, point2d):
+			return vector((self.vector[0]+b.point[0], self.vector[1]+b.point[1]))
+		if isinstance(b, vector):
+			return vector((self.vector[0]+b.vector[0], self.vector[1]+b.vector[1], b.vector[2]))
+		if isinstance(b, vector2d):
+			return vector((self.vector[0]+b.vector[0], self.vector[1]+b.vector[1]))
 
 	def __sub__(self, b):
-		return vector2d((self.vector[0]-b.vector[0], self.vector[1]-b.vector[1]))
+		if isinstance(b, point):
+			return vector((self.vector[0]-b.point[0], self.vector[1]-b.point[1], b.point[2]))
+		if isinstance(b, point2d):
+			return vector((self.vector[0]-b.point[0], self.vector[1]-b.point[1]))
+		if isinstance(b, vector):
+			return vector((self.vector[0]-b.vector[0], self.vector[1]-b.vector[1], b.vector[2]))
+		if isinstance(b, vector2d):
+			return vector((self.vector[0]-b.vector[0], self.vector[1]-b.vector[1]))
 
 	def __mul__(self, b):
 		if isinstance(b, vector):
@@ -94,31 +144,63 @@ class vector2d:
 class vector(object):
 
 	vector = None
-	def __new__(cls, v):
-		if len(v) == 2:
-			if WARNING:
-				print("Creating vector2d object")
-			return vector2d(v)
-		if len(v) == 3:
-			if WARNING:
-				print("Creating vector3d object")
-			return super(vector, cls).__new__(cls)
+	def __new__(cls, v, u=None):
+		if u == None:
+			if isinstance(v, point2d):
+					return vector2d(v.point)
+			if isinstance(v, tuple):
+				if len(v) == 2:
+					return vector2d(v)
+				if len(v) == 3:
+					return super(vector, cls).__new__(cls)
+				assert len(v) in [2, 3], "vector must be either 2 or 3 dimensional."
+			if isinstance(v, (vector2d, point)):
+				return super(vector, cls).__new__(cls)
+		else:
+			if isinstance(v, tuple):
+				if max(len(u), len(v)) == 2:
+					return vector2d(v, u)
+				if max(len(u), len(v)) == 3:
+					return super(vector, cls).__new__(cls)
+			if isinstance(v, (point, point2d)):
+				if max(len(u.point), len(v.point)) == 2:
+					return vector2d(v, u)
+				if max(len(u.point), len(v.point)) == 3:
+					return super(vector, cls).__new__(cls)
 
-	def __init__(self, *args):
-		
-		if len(args) == 1:
-			assert len(args[0]) == 3, "Vector needs to be 3-dimensional"
-			self.vector = args[0]
-
-		if len(args) == 2:
-			v = (args[0] - args[1])
-			self.vector = (v.point[0], v.point[1], v.point[2])
+	def __init__(self, v, u=None):
+		if u == None:
+			if isinstance(v, point):
+				self.vector = v.point
+			if isinstance(v, tuple):
+				self.vector = v
+			if isinstance(v, vector2d):
+				self.vector = v.vector + (0,)
+		else:
+			if isinstance(v, point):
+				self.vector = (v-u).point
+			if isinstance(v, tuple):
+				self.vector = (v[0]-u[0], v[1]-u[1], v[2]-u[2])
 
 	def __add__(self, b):
-		return vector((self.vector[0]+b.vector[0], self.vector[1]+b.vector[1], self.vector[2]+b.vector[2]))
+		if isinstance(b, point):
+			return point((self.vector[0]+b.point[0], self.vector[1]+b.point[1], self.vector[2]+b.point[2]))
+		if isinstance(b, point2d):
+			return point((self.vector[0]+b.point[0], self.vector[1]+b.point[1], self.vector[2]))
+		if isinstance(b, vector):
+			return point((self.vector[0]+b.vector[0], self.vector[1]+b.vector[1], self.vector[2]+b.vector[2]))
+		if isinstance(b, vector2d):
+			return point((self.vector[0]+b.vector[0], self.vector[1]+b.vector[1], self.vector[2]))
 
 	def __sub__(self, b):
-		return vector((self.vector[0]-b.vector[0], self.vector[1]-b.vector[1], self.vector[2]-b.vector[2]))
+		if isinstance(b, point):
+			return point((self.vector[0]-b.point[0], self.vector[1]-b.point[1], self.vector[2]-b.point[2]))
+		if isinstance(b, point2d):
+			return point((self.vector[0]-b.point[0], self.vector[1]-b.point[1], self.vector[2]))
+		if isinstance(b, vector):
+			return point((self.vector[0]-b.vector[0], self.vector[1]-b.vector[1], self.vector[2]-b.vector[2]))
+		if isinstance(b, vector2d):
+			return point((self.vector[0]-b.vector[0], self.vector[1]-b.vector[1], self.vector[2]))
 
 	def __mul__(self, b):
 		if isinstance(b, vector):
@@ -143,10 +225,5 @@ class vector(object):
 
 if __name__ == "__main__":
 
-	p1 = vector((1, 2))
-	p2 = vector((1, 1))
-	# p2 = vector((3, 4))
-	# p3 = vector((5, 6))
-	v3 = p1^p2
-	print(v3)
-	print(type(v3))
+	v1 = vector((1,))
+	print(v1)
